fe48a96026744589da69bb34897884dca0fd5d90
Mateus Borges <mateusguimaraes717@gmail.com>
2025-02-23 16:33:48 -0300

    Add o scrollview no calendario

--- src/screens/Agendar/index.tsx
+++ src/screens/Agendar/index.tsx
@@ -1,10 +1,15 @@
 import { Container } from "./styles";
 import CustomCalendar from "../../components/CustomCalendar/CustomCalendar";
+import {AppHeader} from "../../components/AppHeader/AppHeader";
+import {ScrollView} from "react-native";
 
 export function Agendar() {
     return (
         <Container>
-            <CustomCalendar />
+            <AppHeader />
+            <ScrollView  showsVerticalScrollIndicator={false}>
+                <CustomCalendar />
+            </ScrollView >
         </Container>
     );
 }
