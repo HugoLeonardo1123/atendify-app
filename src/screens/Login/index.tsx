e38b82de9cd02bddf6adaa2ba8f843258e9b5992
Mateus Borges <mateusguimaraes717@gmail.com>
2025-03-31 10:51:55 -0300

    login funcionando

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -11,7 +11,12 @@ import {
 } from './styles';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import * as Google from 'expo-auth-session/providers/google';
+import { makeRedirectUri } from 'expo-auth-session';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
+
 // ios 570429017028-c83tptvl1aepfcl9u0tr6urn0vgdmopq.apps.googleusercontent.com
 // aindroid 570429017028-6n92r66vbp1k9mbfe1umrji6vm2130l0.apps.googleusercontent.com
 // sh1 25:43:6F:97:6F:C0:E4:39:86:12:73:33:DF:A5:0E:02:22:97:6E:F5
@@ -23,7 +28,6 @@ export function Login() {
       '570429017028-6n92r66vbp1k9mbfe1umrji6vm2130l0.apps.googleusercontent.com',
     iosClientId:
       '570429017028-c83tptvl1aepfcl9u0tr6urn0vgdmopq.apps.googleusercontent.com',
-    redirectUri: 'atendify2025://',
   });
 
   return (
